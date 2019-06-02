85ab1cbeca76a19feac3ed35311cc7d5f3bb0e72
Luiz Eduardo <Brayner.luizeduardo@gmail.com>
2019-06-01 22:17:13 -0300

    Prototipo 2

---
+++ Assets/Scripts/Scene/SceneControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneControler : MonoBehaviour
+{
+    public void ClickPlay()
+    {
+        SceneManager.LoadScene("GameFase1");
+    }
+
+    public void ClickOpcoes(){
+        SceneManager.LoadScene("Opções");
+    }
+
+    public void ClickExit()
+    {
+        Application.Quit();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+         SceneManager.LoadScene("GameFase3");
+    }
+
+}

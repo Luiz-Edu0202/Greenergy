2958c0fd8139a86359a21efabb72967cd4bc446c
tucoff <baracho.arturraffael@gmail.com>
2019-06-04 13:44:03 -0300

    Prototipo 4

---
+++ Assets/Scripts/Fase 3/DoorBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour
+{
+    [SerializeField] private bool desenvolvedor;
+    // Start is called before the first frame update
+    public GameObject door;
+    public GameObject door2;
+    public GameObject door;
+    public GameObject door4;
+    public GameObject door5;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Porta();
+    }
+
+    void Porta()
+    {
+        if(desenvolvedor)
+        {
+            Destroy(door1);
+            Destroy(door2, 1f);
+            Destroy(door3, 1f);
+            
+        }
+    }
+
+}
